f49ced14d603d862c399c244263434709978ced3
Cosmo Borsky <cosmo@dabitindustries.com>
2017-06-30 21:32:57 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # lidar_body_tracking
-ROS Catkin package to track people using ortree and cluster extraction
+ROS Catkin package to track people using ortree and cluster extraction  
+Sensor Used for testing: Quanergy M8  
+Written and tested on: Ubuntu 16.04, ROS Kinetic  
+![](images/lidar_tracking.gif)  
+
+## Installation
+1. Make your catkin workspace:
+    1. `mkdir -p ~/catkin_ws/src`
+    2. `catkin_init_workspace ~/catkin_ws/src`
+    3. `catkin_make -C ~/catkin_ws`
+2. Clone this repo to your workspace:
+    1. `git clone https://github.com/MyNameIsCosmo/lidar_body_tracking ~/catkin_ws/src`
+3. Source your workspace
+    1. `source ~/catkin_ws/devel/setup.sh`
+
+## Running the tracking
+1. Initialize your LIDAR, or play your ROSBAG  
+    1. `rosbag play -l bagname.bag`
+2. Launch 
+    1. `source ~/catkin_ws/devel/setup.sh`
+    2. `roslaunch lidar_body_tracking lidar_body_tracking.launch`
+
+## Notes
+1. The URDF frame is QP308. You can change this in `/urdf/m8.launch.xacro`  
+
+## TODO:
+[ ] Dynamic Reconfigure for Link, Leaf size, min cluster, etc  
+[ ] Clustering of indicies for person detection  
+[ ] Output potential people to a topic  
+[ ] Control an RVIZ marker or something  
+[ ] Comment and document code  
+
+## References
+[ROS WIKI URDF](http://wiki.ros.org/urdf)  
+[ROS WIKI Xacro Reference](http://wiki.ros.org/xacro)  
---
+++ src/pcl_body_tracking.cpp
@@ -0,0 +1,92 @@
+#include <ros/ros.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/octree/octree.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/PCLHeader.h>
+
+#include <sensor_msgs/PointCloud2.h>
+
+// Topics
+static const std::string SCAN_TOPIC = "/QP308/pc_QP308";
+static const std::string FILTERED_TOPIC = "/pcl_filtered";
+static const std::string FILTER_TOPIC = "/pcl_filter";
+
+// Variables
+static const int LEAF_SIZE = 10;
+static const float RESOLUTION = 0.1f;
+static const int MIN_FILTERED_CLOUD_SIZE = 50;
+
+// ROS Publisher
+ros::Publisher pub_filtered, pub_filter, pub_clustered;
+bool first_msg = true;
+sensor_msgs::PointCloud2 msg_clustered, msg_filtered, msg_filter;
+pcl::PointCloud<pcl::PointXYZ> cloud, filter;
+pcl::PointCloud<pcl::PointXYZ>::Ptr filter_ptr, cloud_ptr;
+
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+  pcl::PCLPointCloud2 pcl_pc;
+  pcl_conversions::toPCL(*cloud_msg, pcl_pc);
+  pcl::fromPCLPointCloud2(pcl_pc, cloud);
+
+  pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (RESOLUTION);
+  cloud_ptr = cloud.makeShared();
+
+  if (first_msg){
+    first_msg = false;
+    filter_ptr = cloud.makeShared();
+  }
+
+  octree.setInputCloud(filter_ptr);
+  octree.addPointsFromInputCloud();
+
+  octree.switchBuffers();
+
+  octree.setInputCloud(cloud_ptr);
+  octree.addPointsFromInputCloud();
+
+  std::vector<int> newPointVector;
+  octree.getPointIndicesFromNewVoxels(newPointVector, LEAF_SIZE);
+
+  ROS_INFO("indicies %d", newPointVector.size());
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud;
+  filtered_cloud = (pcl::PointCloud<pcl::PointXYZ>::Ptr) new pcl::PointCloud<pcl::PointXYZ>();
+  filtered_cloud->points.reserve(newPointVector.size());
+
+  for (std::vector<int>::iterator it = newPointVector.begin (); it != newPointVector.end (); it++)
+    filtered_cloud->points.push_back(cloud_ptr->points[*it]);
+
+  pcl::toROSMsg(*filtered_cloud, msg_filtered);
+  msg_filtered.header = cloud_msg->header;
+  pub_filtered.publish(msg_filtered);
+
+  pcl::toROSMsg(filter, msg_filter);
+  msg_filter.header = cloud_msg->header;
+  pub_filter.publish(msg_filter);
+
+}
+
+int main (int argc, char** argv)
+{
+  ros::init (argc, argv, "lidar_body_extraction");
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub = nh.subscribe(SCAN_TOPIC, 1, cloud_cb);
+
+  pub_filtered = nh.advertise<sensor_msgs::PointCloud2>(FILTERED_TOPIC, 1);
+  pub_filter = nh.advertise<sensor_msgs::PointCloud2>(FILTER_TOPIC, 1);
+
+  ROS_INFO_STREAM("Spinning");
+
+  ros::spin();
+
+  return 0;
+}
+
